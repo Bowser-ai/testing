8f2145f586d2304ed5c67defd76c843afd4e5c60
Martijn <martijnvermeulen1@gmail.com>
2022-08-23 14:31:46 +0200

    wrote first test spec and imported 'unittest' module

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,21 @@
 from selenium import webdriver
+import unittest
 
-options = webdriver.ChromeOptions()
-options.binary_location = '/usr/bin/brave'
-browser = webdriver.Chrome(options=options)
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self) -> None:
+        options = webdriver.ChromeOptions()
+        options.binary_location = '/usr/bin/brave'
+        self.browser = webdriver.Chrome(options=options)
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self) -> None:
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-do', self.browser.title)
+        self.fail("finish the test")
+
+
+if __name__ == '__main__':
+    unittest.main()
