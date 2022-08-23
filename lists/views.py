385c29f8bfa80f810d79a0060e540e3d69dc7b47
Martijn <martijnvermeulen1@gmail.com>
2022-08-23 14:57:40 +0200

    home page with minimal HTML

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
-def home_page(self) -> HttpResponse:
-    pass
+def home_page(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('<html><title>To-Do lists</title></html>')
